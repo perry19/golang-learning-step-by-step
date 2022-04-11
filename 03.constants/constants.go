15cd9565acf935c4981d11502dc0dd58c20535cd
perry19 <fopaarmel@gmail.com>
2022-04-11 15:36:30 +0100

    We learned how to declare constants using the const keyword

---
+++ 03.constants/constants.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// we use the keyword const to define a constant
+	const PI float64 = math.Pi
+
+	fmt.Println("The value of PI is", PI)
+}
