143c9f94b6293c6264037f9f8d4268749d858a60
perry19 <fopaarmel@gmail.com>
2022-04-13 14:44:29 +0100

    We learned how to create an instantiate structures, accessing fields in strucutures and more

---
+++ the-basics/14.structures/structures.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Go’s structs are typed collections of fields.
+	// They’re useful for grouping data together to form records.
+
+	// Here we create a new type called Animal, which is a structure
+	// and give it some properties
+
+	type Animal struct {
+		name string
+		age  int
+	}
+
+	// To create an instance of Animal we do as follows
+	// We are creating a new Animal (dog) with name bobby and age 3
+
+	dog := Animal{name: "Bobby", age: 3}
+	fmt.Println(dog)  // => {Bobby 3}
+
+	// To get a specific value like the age we can use the dot (.)
+	fmt.Println("Bobby is:", dog.age, "years old")  // => Bobby is: 3 years old
+
+	// we can equally use the new keyword to create a new instance of Animal
+
+	var cat = new(Animal)
+	cat.name = "Chippy"
+	cat.age = 2
+
+	fmt.Println(cat.name, "is", cat.age, "Years old")
+
+	
+}
